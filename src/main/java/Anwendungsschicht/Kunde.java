bc91998b57e9f7004a61db49b8fa1728bca53a62
OmitAigis <macfly.recurve@gmail.com>
2021-11-21 20:10:29 +0100

    Beispielklasse: Kunde

--- src/main/java/Anwendungsschicht/Kunde.java
+++ src/main/java/Anwendungsschicht/Kunde.java
@@ -1,9 +1,19 @@
 package Anwendungsschicht;
 
+import java.util.Hashtable;
+
 public class Kunde {
-
     int Kundennummer;
+    // key = lieferungsnummer
+    // value = lieferung
+    Hashtable Lieferungen;
 
-    public getLieferungsnummer(Lieferungsnummer);
+    public Kunde(int kundennummer, Hashtable lieferungen) {
+        this.Kundennummer = kundennummer;
+        this.Lieferungen = lieferungen;
+    }
 
+    public Lieferung getLieferung(int lieferungsnummer) {
+        return (Lieferung) this.Lieferungen.get(lieferungsnummer);
+    }
 }
